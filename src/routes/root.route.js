91806bc1234bcd00cacf7fe092ad32ec18b8f136
akhang <lnak211099@gmail.com>
2023-04-19 19:50:13 +0700

    user & refactor code

--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -4,7 +4,7 @@ const authRoute = require('./auth.route');
 const projectRoute = require('./project.route');
 const taskRoute = require('./task.route');
 const commentRoute = require('./comment.route');
-// const userRoute = require('./user.route');
+const userRoute = require('./user.route');
 
 const rootRoute = express.Router();
 
@@ -12,7 +12,7 @@ rootRoute.use('/auth', authRoute);
 rootRoute.use('/projects', projectRoute);
 rootRoute.use('/tasks', taskRoute);
 rootRoute.use('/comments', commentRoute);
-// rootRoute.use('/users', userRoute);
+rootRoute.use('/users', userRoute);
 rootRoute.use('*', (_req, res) => notFoundCode(res));
 
 module.exports = rootRoute;
